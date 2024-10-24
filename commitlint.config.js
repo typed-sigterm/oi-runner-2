e4701341197184577b68e60383e427ea8459f79f
Typed SIGTERM <typed.sigterm@gmail.com>
2024-10-25 01:16:35 +0800

    chore: init husky
    [skip ci]

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,6 @@
 const types = ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test'];
 
-export default {
+module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'type-enum': [2, 'always', [...types, 'release']],
