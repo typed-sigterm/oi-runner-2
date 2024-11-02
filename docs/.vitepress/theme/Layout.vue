616d6e7180c542c7f36bb36cae9cfab5149f9760
Typed SIGTERM <typed.sigterm@gmail.com>
2024-11-03 01:02:45 +0800

    revert "docs: migrate from `vitepress-plugin-comment-with-giscus` to `vitepress-giscus`"
    This reverts commit 6a97933463b3a428eca0e2acefdb5db755a0a907.

--- docs/.vitepress/theme/Layout.vue
+++ docs/.vitepress/theme/Layout.vue
@@ -1,23 +1,30 @@
 <script lang="ts" setup>
+import { useData, useRoute } from 'vitepress';
 import DefaultTheme from 'vitepress/theme';
-import { VPGiscus } from 'vitepress-giscus';
+import giscusTalk from 'vitepress-plugin-comment-with-giscus';
 
 const { Layout } = DefaultTheme;
+const { frontmatter, lang } = useData();
+const route = useRoute();
+
+giscusTalk({
+  repo: 'typed-sigterm/oi-runner-2',
+  repoId: 'R_kgDOMMOVrA',
+  category: 'Comments',
+  categoryId: 'DIC_kwDOMMOVrM4Cjq1r',
+  mapping: 'pathname',
+  inputPosition: 'bottom',
+  lang: lang.value,
+  locales: {
+    'zh-Hans': 'zh-CN',
+    'en-US': 'en',
+  },
+}, {
+  frontmatter,
+  route,
+}, true);
 </script>
 
 <template>
-  <Layout>
-    <template #doc-after>
-      <VPGiscus
-        :giscus="{
-          repo: 'typed-sigterm/oi-runner-2',
-          repoId: 'R_kgDOMMOVrA',
-          category: 'Comments',
-          categoryId: 'DIC_kwDOMMOVrM4Cjq1r',
-          mapping: 'pathname',
-          inputPosition: 'bottom',
-        }"
-      />
-    </template>
-  </Layout>
+  <Layout />
 </template>
