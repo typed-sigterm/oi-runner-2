1ec03b74a831c4d21fc1821f84b44d8b07d80278
Typed SIGTERM <typed.sigterm@gmail.com>
2024-11-06 00:51:17 +0800

    docs: migrate from `vitepress-plugin-comment-with-giscus` to `vitepress-giscus`

--- docs/.vitepress/theme/Layout.vue
+++ docs/.vitepress/theme/Layout.vue
@@ -1,30 +1,23 @@
 <script lang="ts" setup>
-import { useData, useRoute } from 'vitepress';
 import DefaultTheme from 'vitepress/theme';
-import giscusTalk from 'vitepress-plugin-comment-with-giscus';
+import { VPGiscus } from 'vitepress-giscus';
 
 const { Layout } = DefaultTheme;
-const { frontmatter, lang } = useData();
-const route = useRoute();
-
-giscusTalk({
-  repo: 'typed-sigterm/oi-runner-2',
-  repoId: 'R_kgDOMMOVrA',
-  category: 'Comments',
-  categoryId: 'DIC_kwDOMMOVrM4Cjq1r',
-  mapping: 'pathname',
-  inputPosition: 'bottom',
-  lang: lang.value,
-  locales: {
-    'zh-Hans': 'zh-CN',
-    'en-US': 'en',
-  },
-}, {
-  frontmatter,
-  route,
-}, true);
 </script>
 
 <template>
-  <Layout />
+  <Layout>
+    <template #doc-after>
+      <VPGiscus
+        :giscus="{
+          repo: 'typed-sigterm/oi-runner-2',
+          repoId: 'R_kgDOMMOVrA',
+          category: 'Comments',
+          categoryId: 'DIC_kwDOMMOVrM4Cjq1r',
+          mapping: 'pathname',
+          inputPosition: 'bottom',
+        }"
+      />
+    </template>
+  </Layout>
 </template>
